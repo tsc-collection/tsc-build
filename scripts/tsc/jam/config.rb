c4261d70921f23fd100b4c16b9adaf8ae3600d16
bystr <bystr@4041ddc3-af0d-0410-b40f-9dc7263b2dfa>
2008-06-30 10:37:11 +0000

    Extening standard jam config with ability to read config/build.yaml file.
    git-svn-id: https://svn.code.sf.net/p/tsc-build/code@147 4041ddc3-af0d-0410-b40f-9dc7263b2dfa

--- scripts/tsc/jam/config.rb
+++ scripts/tsc/jam/config.rb
@@ -1,3 +1,4 @@
+# vi: set sw=2:
 =begin
   Copyright (c) 2008, Quest Software, http://www.quest.com
   
@@ -8,25 +9,100 @@
 =end
 
 require 'tsc/config-locator.rb'
+require 'tsc/platform.rb'
+require 'tsc/config.rb'
+require 'etc'
+require 'enumerator'
 
 module TSC
   module Jam
     class Config
-      def locator 
-        @locator ||= TSC::ConfigLocator.new('.buildrc')
+      def options
+        add_parameter Hash[
+          :JAMCONFIG_PRODUCT_TAG => tag,
+          :JAMCONFIG_PRODUCT_ID => product,
+          :JAMCONFIG_PRODUCT_NAME => name,
+          :JAMCONFIG_PRODUCT_RELEASE => release,
+          :JAMCONFIG_PRODUCT_BUILD => build,
+          :JAMCONFIG_PRODUCT_PLATFORM => platform.name,
+          :JAMCONFIG_LIBRARY_PREFIX => library_prefix,
+          :JAMCONFIG_LIBRARY_MAJOR => library_major,
+          :JAMCONFIG_USER => user
+        ]
+      end
+
+      protected
+      #########
+
+      def add_parameter(*args)
+        array = args.flatten.map { |_item|
+          Array(_item)
+        }
+        array.flatten.each_slice(2).map { |_key, _value|
+          value = _value.to_s.strip
+          [ '-s', "#{_key}=#{value}" ] unless value.empty?
+        }.compact.flatten
+      end
+
+      def product
+        config['product']
+      end
+
+      def tag
+        config['tag']
+      end
+
+      def name
+        config['name']
+      end
+
+      def release
+        config['release']
+      end
+
+      def build
+        config['build']
+      end
+
+      def library_prefix
+        config['library-prefix']
+      end
+
+      def library_major
+        config['library-major']
+      end
+      
+      def user
+        Etc.getpwuid.name rescue Etc.getlogin
+      end
+
+      def platform 
+        platform = TSC::Platform.current
       end
 
       def config
         @config ||= begin
-          locator.find_all_above.inject(Hash[]) { |_memo, _config|
-            _memo.update _config.hash
+          TSC::Config.parse(File.join(File.dirname(config_path), 'build.yaml')).hash rescue Hash.new
+        end
+      end
+
+      def buildrc_locator 
+        @buildrc_locator ||= TSC::ConfigLocator.new('.buildrc')
+      end
+
+      def buildrc
+        @buildrc ||= begin
+          buildrc_locator.find_all_above.inject(Hash[]) { |_memo, _config|
+            _config.hash.each_pair do |_key, _value|
+              _memo[_key.to_s.downcase] = _value
+            end
           }
         end
       end
 
       def environment
         @environment ||= begin
-          config['environment'] || Hash[]
+          buildrc['environment'] || buildrc['make'] || Hash[]
         end
       end
 
