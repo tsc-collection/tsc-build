32a58966ac07e2f3a77e07eedb7cff183408360e
bystr <bystr@4041ddc3-af0d-0410-b40f-9dc7263b2dfa>
2009-04-08 06:26:39 +0000

    Refactored library merging (via reloc objects). DRY-ed static/shared library actions regarding expansions of reloc objects.
    git-svn-id: https://svn.code.sf.net/p/tsc-build/code@170 4041ddc3-af0d-0410-b40f-9dc7263b2dfa

---
+++ scripts/tsc/jam/expand-reloc.rb
@@ -0,0 +1,23 @@
+=begin
+  vim: set sw=2:
+  Copyright (c) 2009, Gennady Bystritsky <bystr@mac.com>
+  
+  Distributed under the MIT Licence.
+  This is free software. See 'LICENSE' for details.
+  You must read and accept the license prior to use.
+  
+  Author: Gennady Bystritsky
+=end
+
+def expand_reloc(top, items)
+  items.map { |_item|
+    case _item
+      when %r{[.]reloc$}
+        expand_reloc top, File.readlines(_item).map { |_line|
+          top + _line.strip
+        }
+      else
+        _item
+    end
+  }.flatten
+end
